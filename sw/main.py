c5beded1197884b0023aa46e3311f2d3be19c458
Chris Moberly <chris@cmoberly.com>
2014-04-22 22:56:49 -0700

    Slight improvement
    Added reset button, cleaned up some code.

--- sw/main.py
+++ sw/main.py
@@ -1,16 +1,8 @@
 from kivy.app import App
 from kivy.clock import Clock
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.scatter import Scatter
 
-milliseconds = 0
-seconds = 0
-minutes = 0
-attempts = 0
-wins = 0
+milliseconds = seconds = minutes = attempts = wins = 0
 running = "No"
 
 class GameScreen(BoxLayout):
@@ -48,6 +40,18 @@ class GameScreen(BoxLayout):
         else:
             secondsf = seconds
         timer_kv.text = (str(minutes) + ":" + str(secondsf) + "." + str(milliseconds))
+
+    def reset(self, *args):
+        timer_kv = self.ids['timer_kv']
+        score_kv = self.ids['score_kv']
+        global milliseconds
+        global seconds
+        global minutes
+        global attempts
+        global wins
+        milliseconds = seconds = minutes = attempts = wins = 0
+        score_kv.text = "0/0"
+        timer_kv.text = "0:00.0"
 
 
 class StopWatchApp(App):
